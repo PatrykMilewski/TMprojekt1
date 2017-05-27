10f365f9a8ada843970b9e24c39246f035be70ae
PatrykMilewski <patryk.milewski@gmail.com>
2017-05-27 23:41:53 +0200

    almost done, turning test led and counting reaction time left, all functions done

--- ConsoleApplication2/ConsoleApplication2/main.c
+++ ConsoleApplication2/ConsoleApplication2/main.c
@@ -15,34 +15,56 @@ int XBYTE[];
 
 #endif // DEBUG
 
+#pragma region CONSTVALUES
+
+#define TRUE 1
+#define FALSE 0
+
 // adres rejestru CSKB1
 #define CSKB1 0xf022
 
 // kod klawisza, ktory sprawdza sprawdza refleks uzytkownika
 #define KLAWISZAKTYWACJI 0x7F
 
-// maksymalny czas, ktory moze wylosowac generator licz losowych
+// maksymalny czas, ktory moze wylosowac generator liczb losowych
 // czas ten okresla, za ile zapali sie dioda
 #define MAXCZASCZEKANIA 3000
 
-// Dla wszystkich opoznien:
-// dlugosc_opoznienia = ( wartoscStalaOpoznienia ^ 2 ) * dlugosc_wykonania_jednej_instrukcji
-// np: dlugosc_opoznienia = ( displayNumberDelay ^ 2 ) * dlugosc_wykonania_jednej_instrukcji
+// minimalny czas, ktory moze wylosowac generator liczb losowych
+// czas ten okresla, za ile zapali sie dioda
+#define MINCZASCZEKANIA 1000
 
-// wartosc, ktora okresla opoznienie
-#define displayNumberDelay 255	
+// stala okreslajaca ile razy musi wykonac sie petla for
+// zeby opoznic program o 1 milisekunde
+#define oneMilisecondConst 1275
 
-// wartosc, ktora okresla czas wyswietlania czasu reakcji na wyswietlaczu
-#define displayReactionTimeDelay 255
+// okresla, ile mozemy wyswietlic maksymalnie cfyr
+#define digitsAmount 6
 
-// random seed, duza liczba pierwsza
-#define randSeed 15486827
+// czas w milisekundach okreslajacy, co ile przelaczamy sie
+// pomiedzy cyframi w wyswietlaczu 7 segmentowym
+// mozna dostosowac jak jest efekt mrugania
+#define displayNumberDelay 10	
 
+// czas w milisekundach potrzebny do wyswietlenia
+// czasu reakcji na wyswietlaczu 7 segmentowym
+#define displayValueDelay (digitsAmount * displayNumberDelay)
+
+// czas w milisekundach okreslajacy, ile czasu bedzie wyswietlany
+// wynik pomiaru czasu reakcji na wyswietlaczu
+#define displayReactionTimePeriod 2000
+
+#pragma endregion
+
+// tablica z cyframi od 0 do 9 dla wyswietlacza 7 segemntowego
 const char numbers[10] = { 0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F };
 
 
-int random() {
-	
+// funkcja pauzujaca program
+void delayProgram(unsigned int value) {
+	unsigned int i = 0, j = 0;
+	for (; i < value; i++)
+		for (; j < oneMilisecondConst; j++);	// czas wykonania wewnetrznej petli = 1 ms
 }
 
 // wyswietlanie podanej wartosci na wyswietlaczu 7 segmentowym
@@ -50,7 +72,7 @@ int random() {
 void displayNumber(unsigned int value) {
 	char numberToDisplay;
 
-	for (char i = 0; i < 6; i++) {
+	for (char i = 0; i < digitsAmount; i++) {
 		numberToDisplay = value % 10;	// bierzemy cyfre jednosci (system dziesietny)
 		value /= 10;					// dzielimy wartosc przez 10, czyli "przesuwamy" calosc o jedna cyfre w prawo
 										// cyfra dziesiatek staje sie cyfra jednosci
@@ -61,25 +83,30 @@ void displayNumber(unsigned int value) {
 		display = 0;
 		
 		// opoznienie pomiedzy cyframi na wyswietlaczu
-		for (short i = 0; i < displayNumberDelay; i++)
-			for (short j = 0; j < displayNumberDelay; j++);
+		delayProgram(displayNumberDelay);
 
 		display = 1;
 	}
 }
 
+// funkcja wyswietlajaca czas reakcji
+void displayReactionTime(unsigned int value) {
+	// ile razy petla musi sie wykonac, zeby czas reakcji wyswietlal sie zadana dlugosc
+	// czyli loopsAmount = jak_dlugo_ma_sie_wyswietlac / ile_czasu_wyswietla_liczbe
+	unsigned int loopsAmount = displayReactionTimePeriod / displayValueDelay;
 
-void displayTime(unsigned int value) {
-	
+	for (unsigned int i = 0; i < loopsAmount; i++) {
+		displayNumber(value);
+	}
 } 
 
 int main() {
 	short iterationsRandom = 0;
-	int waitTime;
+	unsigned int waitTime;
 
 	// nacisnij pierwszy raz klawisz aktywacji, aby pobrac przypadkowa liczbe
 	// potrzebna do inicjalizacji generatora liczb losowych
-	while (1) {
+	while (TRUE) {
 		iterationsRandom++;
 		if (XBYTE[CSKB1] == KLAWISZAKTYWACJI)
 			break;
@@ -89,11 +116,14 @@ int main() {
 	srand(iterationsRandom);
 
 
-	while (1) {
+	while (TRUE) {
 		// losowanie kiedy zapali sie dioda
-		waitTime = rand() % MAXCZASCZEKANIA;
+		// wartosc z przedzialu <MINCZASCZEKANIA, MAXCZASCZEKANIA>
+		waitTime = (rand() % (MAXCZASCZEKANIA + 1)) + MINCZASCZEKANIA;
 
-		while (1) {
+		goToSleep(waitTime);
+
+		while (TRUE) {
 
 			if (XBYTE[CSKB1] == KLAWISZAKTYWACJI);
 
